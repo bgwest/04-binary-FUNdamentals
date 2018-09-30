5bef1b644d1808f478c8ab4ca02d0d614a22cff2
Kris3579 <kristianesvelt@hotmail.com>
2018-09-30 11:04:58 -0700

    redoing tests

--- src/__test__/parse-bitmap.test.js
+++ src/__test__/parse-bitmap.test.js
@@ -17,79 +17,83 @@ describe('Testing transformations', () => {
     expect(Transformer.run('src/assets/house.bmp')).toEqual(70);
   });
 
-  test('darken transformation', (done) => {
+  test('darken transformation, expect: any value of the originalHouse.colorTableBuffer to have been reduced by 125, no lower than 0', (done) => {
     fs.readFile(`${__dirname}/../assets/house.bmp`, (error, buffer) => {
       if (error) {
         throw error;
       }
 
       originalHouse = new BitMapData(buffer);
-      transformedHouse = new BitMapData(buffer, 'darken');
-      transformedHouse.darken();
-
+      transformedHouse = originalHouse.darken();
+      // transformedHouse = new BitMapData(buffer, 'darken');
+      // return originalHouse.darken();
+      // .then(() => {
       for (let i = 0; i < originalHouse.colorTableBuffer; i++) {
-        if (originalHouse.colorTableBuffer[i] < 125) {
+        if (originalHouse.colorTableBuffer[i] > 125) {
           expect(originalHouse.colorTableBuffer[i])
-            .toEqual(transformedHouse.colorTableBuffer[5] - 125);
+            .toEqual(transformedHouse.colorTableBuffer[i] - 125);
         } else {
           expect(transformedHouse.colorTableBuffer[i])
             .toEqual(0);
         }
       }
       done();
+      // });
     });
   });
 
-  test('blackAndWhite transformation', (done) => {
-    fs.readFile(`${__dirname}/../assets/house.bmp`, (error, buffer) => {
-      if (error) {
-        throw error;
-      }
-
-      originalHouse = new BitMapData(buffer);
-      transformedHouse = new BitMapData(buffer, 'blackAndWhite');
-      transformedHouse.blackAndWhite();
-
-      for (let i = 0; i < originalHouse.colorTableBuffer; i++) {
-        if (originalHouse.colorTableBuffer[i] < 100) {
-          expect(transformedHouse.colorTableBuffer[i])
-            .toEqual(0);
-        } else {
-          expect(transformedHouse.colorTableBuffer[i])
-            .toEqual(255);
-        }
-      }
-      done();
-    });
-  });
-
-  test('blueScale transformation', (done) => {
-    fs.readFile(`${__dirname}/../assets/house.bmp`, (error, buffer) => {
-      if (error) {
-        throw error;
-      }
-
-      originalHouse = new BitMapData(buffer);
-      transformedHouse = new BitMapData(buffer, 'blueScale');
-      transformedHouse.blueScale();
-
-      expect(transformedHouse.colorTableBuffer[2])
-        .toEqual(0);
-      expect(transformedHouse.colorTableBuffer[6])
-        .toEqual(0);
-      expect(transformedHouse.colorTableBuffer[10])
-        .toEqual(0);
-      expect(transformedHouse.colorTableBuffer[18])
-        .toEqual(0);
-      expect(transformedHouse.colorTableBuffer[30])
-        .toEqual(0);
-      expect(transformedHouse.colorTableBuffer[1])
-        .toEqual(originalHouse.colorTableBuffer[1]);
-      expect(transformedHouse.colorTableBuffer[7])
-        .toEqual(originalHouse.colorTableBuffer[7]);
-      expect(transformedHouse.colorTableBuffer[13])
-        .toEqual(originalHouse.colorTableBuffer[13]);
-      done();
-    });
-  });
+  // test('blackAndWhite transformation', (done) => {
+  //   fs.readFile(`${__dirname}/../assets/house.bmp`, (error, buffer) => {
+  //     if (error) {
+  //       throw error;
+  //     }
+  //
+  //     originalHouse = new BitMapData(buffer);
+  //     transformedHouse = new BitMapData(buffer, 'blackAndWhite');
+  //     return transformedHouse.blackAndWhite()
+  //       .then(() => {
+  //         for (let i = 0; i < originalHouse.colorTableBuffer; i++) {
+  //           if (originalHouse.colorTableBuffer[i] < 100) {
+  //             expect(transformedHouse.colorTableBuffer[i])
+  //               .toEqual(0);
+  //           } else {
+  //             expect(transformedHouse.colorTableBuffer[i])
+  //               .toEqual(255);
+  //           }
+  //         }
+  //         done();
+  //       });
+  //   });
+  // });
+  //
+  // test('blueScale transformation', (done) => {
+  //   fs.readFile(`${__dirname}/../assets/house.bmp`, (error, buffer) => {
+  //     if (error) {
+  //       throw error;
+  //     }
+  //
+  //     originalHouse = new BitMapData(buffer);
+  //     transformedHouse = new BitMapData(buffer, 'blueScale');
+  //     return transformedHouse.blueScale()
+  //       .then(() => {
+  //         expect(transformedHouse.colorTableBuffer[2])
+  //           .toEqual(0);
+  //         expect(transformedHouse.colorTableBuffer[6])
+  //           .toEqual(0);
+  //         expect(transformedHouse.colorTableBuffer[10])
+  //           .toEqual(0);
+  //         expect(transformedHouse.colorTableBuffer[18])
+  //           .toEqual(0);
+  //         expect(transformedHouse.colorTableBuffer[30])
+  //           .toEqual(0);
+  //         expect(transformedHouse.colorTableBuffer[1])
+  //           .toEqual(originalHouse.colorTableBuffer[1]);
+  //         expect(transformedHouse.colorTableBuffer[7])
+  //           .toEqual(originalHouse.colorTableBuffer[7]);
+  //         expect(transformedHouse.colorTableBuffer[13])
+  //           .toEqual(originalHouse.colorTableBuffer[13]);
+  //         done();
+  //       });
+  //   });
+  // });
 });
